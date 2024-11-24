00272d14df7dcef0579d3d419b9002bded39712b
Ilyosbek Nishonov <98635769+nishonow@users.noreply.github.com>
2024-11-24 16:22:16 +0600

    Add time limit for message sending

--- admin.py
+++ admin.py
@@ -1,7 +1,7 @@
 from aiogram import types, executor
 from aiogram.dispatcher import FSMContext
 from aiogram.types import Message, CallbackQuery, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton
-
+import asyncio
 from config import ADMINS
 from db import user_exists, add_user, count_users, get_all_user_ids
 from loader import dp, bot
@@ -18,7 +18,7 @@ cancel = InlineKeyboardMarkup(
 
 @dp.message_handler(user_id=ADMINS, text='/admin')
 async def welcome_admin(message: Message):
-    await message.answer("Welcome admin what do you want?", reply_markup=menu)
+    await message.answer("Добро пожаловать, администратор, что вы хотите?", reply_markup=menu)
 
 
 @dp.message_handler(user_id=ADMINS, text='Пользователи')
@@ -44,10 +44,13 @@ async def msg_to_all(message: types.Message, state: FSMContext):
     from_chat = message.from_user.id
     success = 0
     error = 0
-    for id in total_users_id:
+    for idx, id in enumerate(total_users_id):
         try:
             await bot.copy_message(id, from_chat, msg_id)
             success += 1
+            
+            if idx % 30 == 0:
+                await asyncio.sleep(1)
 
         except:
             error += 1
